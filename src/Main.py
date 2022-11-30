5d34691966745cde41cdc9a3da0a762df075f3e4
israncho <isragutierrez@ciencias.unam.mx>
2022-11-29 18:41:28 -0600

    more robust game

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,22 @@
 import Connect4
 
+
+def user_input() -> int:
+    not_finished = True
+    while not_finished:
+        usr_input = input()
+        usr_int = -1
+        try:
+            usr_int = int(usr_input)
+        except:
+            print("Must enter a number!!\n\nTry again: ",end="")
+            continue
+        if usr_int > 6 or usr_int < 0:
+            print("Invalid number!!\n\nTry again: ",end="")
+            continue
+        not_finished = False 
+    return usr_int 
+
 game = Connect4.connect4()
 
 player1 = True
@@ -11,7 +28,7 @@ while game.finished() == 0:
     else:
         print("2", end="")
     print(" make a play: ", end="")
-    play = input()
+    play = user_input() 
     game.make_play(player1, int(play))
     if player1:
         player1 = False
